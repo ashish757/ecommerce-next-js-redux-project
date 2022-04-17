dc44410457d28b54cf22ddf6c7f545a7217fe599
Ashish Raj Singh <75162873+ashish757@users.noreply.github.com>
2022-04-17 10:11:50 +0530

    ju

--- next.config.js
+++ next.config.js
@@ -4,7 +4,7 @@ const nextConfig = {
   images: {
     domains: ['fakestoreapi.com'],
   },
-  presets: ["next/babel"]
+  swcMinify: false // it should be false by default 
 }
 
 module.exports = nextConfig
